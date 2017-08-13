eba2b9de5dda227f6d1bd0087ebf8d0d1de16897
Nate Turner <nater1992@gmail.com>
2017-08-13 08:55:40 -0700

    allow users to add income streams

--- src/Budget.js
+++ src/Budget.js
@@ -33,7 +33,7 @@ class BudgetExpense extends Component {
 }
 
 const fetchBudget = (budgetId) => {
-    return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
         request(`${apiHost}/budget/${budgetId}/`, function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -53,6 +53,9 @@ class Budget extends Component {
             budgetId: "",
             newExpenseName: "",
             newExpenseAmount: "",
+            newIncomeStreamName: "",
+            newIncomeStreamAmount: "",
+            newIncomeStreamFrequency: "",
         }
     }
 
@@ -76,6 +79,18 @@ class Budget extends Component {
         this.setState({newExpenseAmount: event.target.value});
     }
 
+    changeNewIncomeStreamName(event) {
+        this.setState({newIncomeStreamName: event.target.value});
+    }
+
+    changeNewIncomeStreamAmount(event) {
+        this.setState({newIncomeStreamAmount: event.target.value});
+    }
+
+    changeNewIncomeStreamFrequency(event) {
+        this.setState({newIncomeStreamFrequency: event.target.value});
+    }
+
     createNewExpense(event) {
         event.preventDefault()
 
@@ -90,14 +105,33 @@ class Budget extends Component {
                     name: newExpenseName,
                     amount: parseFloat(newExpenseAmount),
                 }
-            }, function (error, response, body) {
-                debugger
-                console.log('error:', error); // Print the error if one occurred
-                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                console.log('body:', body); // Print the HTML for the Google homepage.
-
+            },
+            () => {
                 loadBudget()
                 clearNewExpenseForm()
+            }
+        );
+    }
+
+    createNewIncomeStream(event) {
+        event.preventDefault()
+
+        const loadBudget = this.loadBudget.bind(this)
+        const clearNewIncomeStreamForm = this.clearNewIncomeStreamForm.bind(this)
+        const {newIncomeStreamName, newIncomeStreamAmount, newIncomeStreamFrequency, budgetId} = this.state
+
+        request.post(
+            `${apiHost}/budget/${budgetId}/incomeStream/`,
+            {
+                form: {
+                    name: newIncomeStreamName,
+                    amount: parseFloat(newIncomeStreamAmount),
+                    frequency: newIncomeStreamFrequency,
+                }
+            },
+            () => {
+                loadBudget()
+                clearNewIncomeStreamForm()
             }
         );
     }
@@ -106,6 +140,14 @@ class Budget extends Component {
         this.setState({
             newExpenseAmount: "",
             newExpenseName: "",
+        });
+    }
+
+    clearNewIncomeStreamForm() {
+        this.setState({
+            newIncomeStreamAmount: "",
+            newIncomeStreamName: "",
+            newIncomeStreamFrequency: "",
         });
     }
 
@@ -156,10 +198,10 @@ class Budget extends Component {
                                 {budgetIncomeStreams}
                                 {this.state.incomeStreams.length % 2 === 0 && <tr><td>&nbsp;</td><td/><td/><td/></tr>}
                                 <tr>
-                                    <td><input type="text" value={this.state.newExpenseName} onChange={this.changeNewExpenseName.bind(this)} placeholder="New income stream name"/></td>
-                                    <td><input type="text" value={this.state.newExpenseAmount} onChange={this.changeNewExpenseAmount.bind(this)} placeholder="1000"/></td>
-                                    <td><input type="text" value={this.state.newExpenseAmount} onChange={this.changeNewExpenseAmount.bind(this)} placeholder="2"/></td>
-                                    <td><button onClick={this.createNewExpense.bind(this)}>Create</button></td>
+                                    <td><input type="text" value={this.state.newIncomeStreamName} onChange={this.changeNewIncomeStreamName.bind(this)} placeholder="New income stream name"/></td>
+                                    <td><input type="text" value={this.state.newIncomeStreamAmount} onChange={this.changeNewIncomeStreamAmount.bind(this)} placeholder="1000"/></td>
+                                    <td><input type="text" value={this.state.newIncomeStreamFrequency} onChange={this.changeNewIncomeStreamFrequency.bind(this)} placeholder="2"/></td>
+                                    <td><button onClick={this.createNewIncomeStream.bind(this)}>Create</button></td>
                                 </tr>
                             </tbody>
                         </table>
