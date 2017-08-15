c899c0f39889bce919648125b2d6d2e41cb46b89
Nate Turner <nater1992@gmail.com>
2017-08-14 20:22:22 -0700

    add navigation using react-router

--- src/App.js
+++ src/App.js
@@ -1,170 +1,31 @@
 import React, {Component} from 'react';
 import './App.css';
-import Budget from './Budget.js'
-import request from 'request'
-import {apiHost} from './config'
+import BudgetController from './BudgetController.js'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
-const budget = {
-    "expenses": [
-        {
-            "amount": -1300,
-            "key": 1,
-            "name": "Mortgage"
-        },
-        {
-            "amount": -400,
-            "key": 2,
-            "name": "HOA"
-        },
-        {
-            "amount": -120,
-            "key": 3,
-            "name": "Phone"
-        },
-        {
-            "amount": -60,
-            "key": 4,
-            "name": "Internet"
-        }
-    ],
-    "incomeStreams": [
-        {
-            "amount": 2000,
-            "frequency": 2,
-            "key": 1,
-            "name": "Paycheck"
-        },
-        {
-            "amount": 200,
-            "frequency": 1,
-            "key": 2,
-            "name": "Investment Income"
-        }
-    ]
-}
+import {createBrowserHistory} from 'history'
 
-
-const fetchBudgetIds = () => {
-    return new Promise((resolve, reject) => {
-        request(`${apiHost}/budgets`, function (error, response, body) {
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-
-            resolve(JSON.parse(body)["budget_ids"]);
-        });
-    });
-}
+const Home = () => <div>Home</div>
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            activeBudgetId: "",
-            newBudgetName: "",
-            budgets: [], // All available budget ids
-        }
-    }
-
-    changeActiveBudgetId(event) {
-        this.setActiveBudgetId(event.target.value);
-    }
-
-    setActiveBudgetId(budgetId) {
-        this.setState({activeBudgetId: budgetId});
-    }
-
-    componentDidMount() {
-        this.reloadAllBudgets()
-    }
-
-    reloadAllBudgets() {
-        return fetchBudgetIds()
-            .then(budgets => {
-                this.setState({budgets})
-            })
-    }
-
-    changeNewBudgetName(event) {
-        this.setState({newBudgetName: event.target.value});
-    }
-
-    createBudget(event) {
-        const setActiveBudgetId = this.setActiveBudgetId.bind(this)
-        const reloadAllBudgets = this.reloadAllBudgets.bind(this)
-        const newBudgetName = this.state.newBudgetName
-
-        request.post(
-            `${apiHost}/budget/`,
-            {
-                form: {
-                    name: newBudgetName,
-                }
-            },
-            (err, httpResponse, body) => {
-                reloadAllBudgets()
-                    .then(() => {
-                        debugger
-                        setActiveBudgetId(JSON.parse(body)["id"])
-                    })
-            }
-        );
-    }
-
-    deleteBudget = () => {
-        const budgetId = parseInt(this.state.activeBudgetId)
-
-        const budgetWithDeletedBudgetRemoved =
-            this.state.budgets
-                .filter(budget => budget.id !== budgetId)
-
-        this.setState({
-            budgets: budgetWithDeletedBudgetRemoved,
-            activeBudgetId: "",
-        })
-
-        request.delete(
-            {url: `${apiHost}/budgets/${budgetId}`},
-            (error, httpResponse, body) => {}
-        )
-    }
-
     render() {
-        const {budgets, activeBudgetId} = this.state
-
-        const budgetOptions = this.state.budgets.map(budget => (
-            <option key={budget.id} value={budget.id}>{ budget.name }</option>
-        ))
-
         return (
+            <Router history={createBrowserHistory()}>
+                <div className="App">
+                    <header>
+                        <Link to="/home">My Money Manager</Link>&nbsp;&nbsp;&nbsp;<Link to="/budgets">Budgets</Link>
+                    </header>
 
-            <div className="App">
-                <header>
-                    My Money Manager - Budget
-                </header>
-
-                <div className="container">
-                    <div className="nav">
-                        Select A Budget:<br/>
-                        <select value={this.state.activeBudgetId} onChange={this.changeActiveBudgetId.bind(this)}>
-                            <option>Select a budget</option>
-                            {budgetOptions}
-                        </select>
-                        {activeBudgetId !== "" && <button onClick={this.deleteBudget.bind(this)}>Delete Budget</button>}
-
-                        <br />
-                        <br />
-                        Create New Budget:
-                        <input type="text" value={this.state.newBudgetName}
-                               onChange={this.changeNewBudgetName.bind(this)}
-                               placeholder="New budget name" />
-                        <button onClick={this.createBudget.bind(this)}>Create</button>
-                    </div>
-                    <div className="main">
-                        {activeBudgetId && <Budget budgetId={activeBudgetId}/>}
-                    </div>
+                    <Route exact path="/" component={BudgetController}/>
+                    <Route exact path="/home" component={Home}/>
+                    <Route path="/budgets" component={BudgetController}/>
+                    <Route path="/budget/:budgetId" component={BudgetController}/>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
---
+++ src/BudgetController.js
@@ -0,0 +1,119 @@
+import React, {Component} from 'react';
+import './App.css';
+import Budget from './Budget.js'
+import request from 'request'
+import {apiHost} from './config'
+import { Link } from 'react-router-dom'
+
+const fetchBudgetIds = () => {
+    return new Promise((resolve, reject) => {
+        request(`${apiHost}/budgets`, function (error, response, body) {
+            resolve(JSON.parse(body)["budget_ids"]);
+        });
+    });
+}
+
+export default class BudgetController extends Component {
+    constructor(props) {
+        super(props)
+        const maybeBudgetId = props.match.params.budgetId
+        this.state = {
+            activeBudgetId: maybeBudgetId !== undefined ? maybeBudgetId : "",
+            newBudgetName: "",
+            budgets: [], // All available budget ids
+        }
+    }
+
+    setActiveBudgetId(budgetId) {
+        this.setState({activeBudgetId: budgetId});
+    }
+
+    componentDidMount() {
+        this.reloadAllBudgets()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const maybeNewBudgetId = this.props.match.params.budgetId
+
+        if (maybeNewBudgetId !== undefined
+            && maybeNewBudgetId !== prevState.activeBudgetId) {
+            this.setActiveBudgetId(maybeNewBudgetId)
+        }
+    }
+
+    reloadAllBudgets() {
+        return fetchBudgetIds()
+            .then(budgets => {
+                this.setState({budgets})
+            })
+    }
+
+    changeNewBudgetName(event) {
+        this.setState({newBudgetName: event.target.value});
+    }
+
+    createBudget(event) {
+        const setActiveBudgetId = this.setActiveBudgetId.bind(this)
+        const reloadAllBudgets = this.reloadAllBudgets.bind(this)
+        const newBudgetName = this.state.newBudgetName
+        const pushHistory = this.props.history.push
+
+        request.post(
+            `${apiHost}/budget/`,
+            {
+                form: {
+                    name: newBudgetName,
+                }
+            },
+            (err, httpResponse, body) => {
+                reloadAllBudgets()
+                    .then(() => {
+                        const newlyCreatedBudgetID = JSON.parse(body)["id"]
+                        pushHistory(`/budget/${newlyCreatedBudgetID}`)
+                    })
+            }
+        );
+    }
+
+    deleteBudget = () => {
+        const budgetId = parseInt(this.state.activeBudgetId)
+
+        const pushHistory = this.props.history.push
+
+        request.delete(
+            {url: `${apiHost}/budgets/${budgetId}`},
+            (error, httpResponse, body) => {
+                pushHistory("/budgets")
+            }
+        )
+    }
+
+    render() {
+        const {budgets, activeBudgetId} = this.state
+
+        const budgetLinks = this.state.budgets.map(budget => (
+            <div key={budget.id}><Link to={`/budget/${budget.id}`}>{ budget.name }</Link></div>
+        ))
+
+        return (
+            <div className="container">
+                <div className="nav">
+                    Select A Budget:<br/>
+                    {budgetLinks}
+                    {activeBudgetId !== "" && <button onClick={this.deleteBudget.bind(this)}>Delete Budget</button>}
+
+                    <br />
+                    <br />
+                    Create New Budget:
+                    <input type="text" value={this.state.newBudgetName}
+                           onChange={this.changeNewBudgetName.bind(this)}
+                           placeholder="New budget name" />
+                    <button onClick={this.createBudget.bind(this)}>Create</button>
+                </div>
+                <div className="main">
+                    {activeBudgetId && <Budget budgetId={activeBudgetId}/>}
+                </div>
+            </div>
+        )
+    }
+}
